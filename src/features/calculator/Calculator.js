785dc89b43c614fb385c4caea3a8c3eaa05af320
Cryptojoint <a.hamdy.nig@gmail.com>
2021-10-31 23:40:57 +0200

    Style update & a little JS -updated operations buttons style -displayed the clicked number on the output screen -"the calculator app re-renders whenever a number is clicked"!

--- src/features/calculator/Calculator.js
+++ src/features/calculator/Calculator.js
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import './Calculator.css';
 
 export const Calculator = () => {
+	const [n1, setN1] = useState('Hello');
+
 	const row0 = ['AC', '+/-', '%', '/'].map((el) => <CalculatorBtn key={el} val={el} />);
 	const row1 = [7, 8, 9, 'X'].map((el) => <CalculatorBtn key={el} val={el} />);
 	const row2 = [4, 5, 6, '-'].map((el) => <CalculatorBtn key={el} val={el} />);
@@ -18,12 +20,22 @@ export const Calculator = () => {
 				console.log(`Button Clicked: ${ev.target.value}`);
 			});
 		});
+
+		console.log(document.querySelectorAll('.numberBtn'));
+		const numberBtns = Array.from(document.querySelectorAll('.numberBtn'));
+		numberBtns.forEach((btn) => {
+			btn.addEventListener('click', (ev) => {
+				console.log(`numberBtn Clicked: ${ev.target.value}`);
+				let _n1 = n1 === 'Hello' ? '' : n1;
+				setN1(_n1 + '' + ev.target.value);
+			});
+		});
 	});
 
 	return (
 		<Container className="calculator">
 			<Row className="output-row">
-				<Col>HELLO</Col>
+				<Col>{n1}</Col>
 			</Row>
 			<Row className="operations-row">{row0}</Row>
 
@@ -32,7 +44,7 @@ export const Calculator = () => {
 			<Row className="operations-row">{row3}</Row>
 			<Row className="operations-row">
 				<Col md={{ span: 6 }}>
-					<Button className="calculatorBtn" value={0}>
+					<Button className="numberBtn" value={0}>
 						0
 					</Button>
 				</Col>
@@ -43,9 +55,22 @@ export const Calculator = () => {
 };
 
 const CalculatorBtn = ({ val }) => {
+	console.log(`${val}: ${!isNaN(val)}`);
+	let _className;
+	if (isNaN(val)) {
+		if (val === '=') {
+			_className = 'equalsBtn';
+		} else if (val === '.') {
+			_className = 'dotBtn';
+		} else {
+			_className = 'operationBtn';
+		}
+	} else {
+		_className = 'numberBtn';
+	}
 	return (
 		<Col md={{ span: 3 }}>
-			<Button className="calculatorBtn" value={val}>
+			<Button className={_className} value={val}>
 				{val}
 			</Button>
 		</Col>
