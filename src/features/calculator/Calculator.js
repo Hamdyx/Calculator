725ab09d582ea0cb180d2932163284818b89c381
Cryptojoint <a.hamdy.nig@gmail.com>
2022-02-22 01:45:14 +0200

    Added media queries -added media queries to be responsive on all devices -updated some css flex boxes

--- src/features/calculator/Calculator.js
+++ src/features/calculator/Calculator.js
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import './Calculator.css';
-
+// @bug every component in Calculator rerenderes
+// 	whenever a number is clicked or a key is pressed
 export const Calculator = () => {
 	const [n1, setN1] = useState('Hello');
 
-	const row0 = ['AC', '+/-', '%', '/'].map((el) => <CalculatorBtn key={el} val={el} />);
+	const row0 = ['C', '%', 'x', '/'].map((el) => (
+		<CalculatorBtn key={el} val={el} />
+	));
 	const row1 = [7, 8, 9, 'X'].map((el) => <CalculatorBtn key={el} val={el} />);
 	const row2 = [4, 5, 6, '-'].map((el) => <CalculatorBtn key={el} val={el} />);
 	const row3 = [1, 2, 3, '+'].map((el) => <CalculatorBtn key={el} val={el} />);
 	const row4 = ['.', '='].map((el) => <CalculatorBtn key={el} val={el} />);
 
 	useEffect(() => {
-		console.log(document.querySelectorAll('.calculatorBtn'));
+		// console.log(document.querySelectorAll('.calculatorBtn'));
 		const calcBtns = Array.from(document.querySelectorAll('.calculatorBtn'));
 		calcBtns.forEach((btn) => {
 			btn.addEventListener('click', (ev) => {
@@ -21,21 +24,58 @@ export const Calculator = () => {
 			});
 		});
 
-		console.log(document.querySelectorAll('.numberBtn'));
+		function appendNumber(n) {
+			console.log(`Number Entered: ${n}`);
+			// selects related dom element
+			// const node = document.querySelector(`button[value="${n}"]`);
+			let _n1 = n1 === 'Hello' ? '' : n1;
+			console.log(n1);
+			console.log(_n1);
+			console.log(n);
+			console.log(_n1 + '' + n);
+			_n1 = _n1 + '' + n;
+			console.log(_n1);
+			// @bug n1 is always 'Hello'
+			setN1(_n1);
+		}
+
+		function enterPressed() {
+			console.log(`enterPressed`);
+		}
+
+		function keyPressed(ev) {
+			console.log(`keyPressed`);
+			console.log(ev.key);
+			if (!isNaN(ev.key) && ev.key !== ' ') appendNumber(ev.key);
+			if (ev.key === 'Enter') enterPressed();
+		}
+
+		// handles when user press Numpad or Digits (0-9)
+		window.addEventListener('keydown', keyPressed);
+
 		const numberBtns = Array.from(document.querySelectorAll('.numberBtn'));
 		numberBtns.forEach((btn) => {
 			btn.addEventListener('click', (ev) => {
 				console.log(`numberBtn Clicked: ${ev.target.value}`);
-				let _n1 = n1 === 'Hello' ? '' : n1;
-				setN1(_n1 + '' + ev.target.value);
+				appendNumber(ev.target.value);
+				// removes focus from clicked element as it causes a bug with the keydown ev
+				// if 'enter' key used while there is a focused element
+				ev.target.blur();
+				/* let _n1 = n1 === 'Hello' ? '' : n1;
+				setN1(_n1 + '' + ev.target.value); */
 			});
 		});
-	});
+	}, []);
 
 	return (
 		<Container className="calculator">
 			<Row className="output-row">
-				<Col>{n1}</Col>
+				<Col>
+					<p>
+						<small>prev number</small>
+					</p>
+					{n1}
+				</Col>
 			</Row>
 			<Row className="operations-row">{row0}</Row>
 
@@ -55,7 +95,6 @@ export const Calculator = () => {
 };
 
 const CalculatorBtn = ({ val }) => {
-	console.log(`${val}: ${!isNaN(val)}`);
 	let _className;
 	if (isNaN(val)) {
 		if (val === '=') {
